9cee9f7a7a6400664ea1ca1c9dbccd98146755fe
Mars1836 <hauhpll123@gmail.com>
2025-02-28 18:56:09 +0700

    fix login

--- client/components/register-form.tsx
+++ client/components/register-form.tsx
@@ -9,7 +9,7 @@ import { Input } from "@/components/ui/input"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import * as z from "zod"
-import { useToast } from "@/components/ui/use-toast"
+import { useToast } from "@/hooks/use-toast"
 
 const formSchema = z
   .object({
