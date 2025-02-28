9dc75563985b91eef5d372f594fae9312c80ebd2
Mars1836 <hauhpll123@gmail.com>
2025-02-28 18:15:53 +0700

    a

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -1,7 +1,7 @@
-import { io } from "socket.io-client"
+import { io } from "socket.io-client";
 
 // Create a socket instance
-const socket = io("http://localhost:3000", {
+const socket = io(process.env.NEXT_PUBLIC_BACKEND_URL, {
   reconnectionDelay: 1000,
   reconnection: true,
   reconnectionAttempts: 10,
@@ -9,20 +9,19 @@ const socket = io("http://localhost:3000", {
   agent: false,
   upgrade: false,
   rejectUnauthorized: false,
-})
+});
 
 // Add event listeners for connection
 socket.on("connect", () => {
-  console.log("Connected to Socket.IO server")
-})
+  console.log("Connected to Socket.IO server");
+});
 
 socket.on("disconnect", () => {
-  console.log("Disconnected from Socket.IO server")
-})
+  console.log("Disconnected from Socket.IO server");
+});
 
 socket.on("connect_error", (error) => {
-  console.log("Connection error:", error)
-})
+  console.log("Connection error:", error);
+});
 
-export default socket
-
+export default socket;
